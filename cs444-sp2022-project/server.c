97e3b72bed6411adb22a39ecebadde814231b0ea
ColinMcDonough <70282607+ColinMcDonough@users.noreply.github.com>
2022-04-21 16:04:02 -0400

    Update server.c

--- cs444-sp2022-project/server.c
+++ cs444-sp2022-project/server.c
@@ -266,12 +266,6 @@ void load_all_sessions() {
  * @param session_id the session ID
  */
 void save_session(int session_id, char session_info[]) {
-    char path[BUFFER_LEN];
-    get_session_file_path(session_id, path);
-    FILE *file;
-    file = fopen(path, "w+");
-    fputs(session_info, file);
-    fclose(file);
     // TODO: For Part 1.1, write your file operation code here.
     // Hint: Use get_session_file_path() to get the file path for each session.
 }
